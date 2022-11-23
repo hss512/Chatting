f6bbde954032eb78add42fa110c3008a921258dc
Seong su Ha <hss25350@naver.com>
2022-11-23 14:34:49 +0900

    test

--- src/main/java/com/gamego/chatting/controller/ChattingController.java
+++ src/main/java/com/gamego/chatting/controller/ChattingController.java
@@ -66,7 +66,7 @@ public class ChattingController {
 
         log.info("===========================================");
 
-        log.info("Enter_message = " , message.getPayload().getMessage());
+        log.info("Enter_message = " + message.getPayload().getMessage());
 
         log.info("ChatController.chatRoomEnter 호출");
 
