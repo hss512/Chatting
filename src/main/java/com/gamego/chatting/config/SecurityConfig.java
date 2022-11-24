9c26fdf4bc385eb0d5054c44c4e079ec69a83993
Seong su Ha <73016759+hss512@users.noreply.github.com>
2022-11-24 18:41:09 +0900

    Update SecurityConfig.java

--- src/main/java/com/gamego/chatting/config/SecurityConfig.java
+++ src/main/java/com/gamego/chatting/config/SecurityConfig.java
@@ -1,4 +1,42 @@
 package com.gamego.chatting.config;
 
-public class SecurityConfig {
-}
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/swagger-ui/**","/webjars/**","/css/**","/js/**","/v2/api-docs","/v3/api-docs" ,"/configuration/ui",
+                "/swagger-resources/**", "/configuration/security",
+                "/swagger-ui.html", "/webjars/**","/swagger/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .formLogin().disable()// formLogin 인증방법 비활성화
+                .httpBasic().disable()// httpBasic 인증방법 비활성화(특정 리소스에 접근할 때 username과 password 물어봄)
+                .cors().disable()
+                .csrf().disable()
+                .headers()
+                .frameOptions().sameOrigin()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/ws/**/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().permitAll();
+    }
