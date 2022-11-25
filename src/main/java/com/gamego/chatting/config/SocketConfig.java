c08d476637f0b194a15b44a7e79cbc07e86050f3
Seong su Ha <73016759+hss512@users.noreply.github.com>
2022-11-25 20:47:58 +0900

    Update SocketConfig.java

--- src/main/java/com/gamego/chatting/config/SocketConfig.java
+++ src/main/java/com/gamego/chatting/config/SocketConfig.java
@@ -18,7 +18,7 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/alarm", "/ws/chat").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws/alarm", "/ws/chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
